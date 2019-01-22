bcf1444d420914c38333f7ba85846ce45c94efce
Loren Mucha <lorenmucha@gmail.com>
2019-01-22 10:33:32 +0100

    add mail support

--- frontend/src/helper.js
+++ frontend/src/helper.js
@@ -70,5 +70,18 @@ const helper={
     slideUpElementByID:function(id){
         $('#'+id).slideUp("slow", function () {});
         return true;
+    },
+    checkIE:function(){
+        var ua = window.navigator.userAgent;
+        var msie = ua.indexOf("MSIE ");
+
+        if (msie > 0 || !!navigator.userAgent.match(/Trident.*rv\:11\./))  // If Internet Explorer, return version number
+        {
+            return true;
+        }
+        else  // If another browser, return 0
+        {
+            return false;
+        }
     }
 };
--- frontend/src/track.js
+++ frontend/src/track.js
@@ -1,28 +1,17 @@
 $(function(){
     //Tracking
-    if(!view_state.getProductionState()){
-        var script_google = `<!-- Global site tag (gtag.js) - Google Analytics -->
-                                <script async src="https://www.googletagmanager.com/gtag/js?id=UA-112688592-1"></script>
-                                <script>
-                                  window.dataLayer = window.dataLayer || [];
-                                  function gtag(){dataLayer.push(arguments);}
-                                  gtag('js', new Date());
-                                  gtag('config', 'UA-112688592-1');
-                                </script>
-                                `;
-        var script_matamo = `<script type="text/javascript">
-                                var _paq = _paq || [];
-                                _paq.push(['trackPageView']);
-                                _paq.push(['enableLinkTracking']);
-                                (function() {
-                                var u="https://www.ioer.de/stats/";
-                                _paq.push(['setTrackerUrl', u+'piwik.php']);
-                                _paq.push(['setSiteId', '4']);
-                                var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
-                                g.type='text/javascript'; g.async=true; g.defer=true; g.src=u+'piwik.js'; s.parentNode.insertBefore(g,s);
-                                })();
-                            </script>`;
+    var script_matamo = `<script type="text/javascript">
+                            var _paq = _paq || [];
+                            _paq.push(['trackPageView']);
+                            _paq.push(['enableLinkTracking']);
+                            (function() {
+                            var u="https://www.ioer.de/stats/";
+                            _paq.push(['setTrackerUrl', u+'piwik.php']);
+                            _paq.push(['setSiteId', '4']);
+                            var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+                            g.type='text/javascript'; g.async=true; g.defer=true; g.src=u+'piwik.js'; s.parentNode.insertBefore(g,s);
+                            })();
+                        </script>`;
 
-        $('head').append(script_google+script_matamo);
-    }
+    $('head').append(script_matamo);
 });
